c74edcadd056176b5118df6688cd0b4950c95a1d
oleg-new <9213195@gmail.com>
2021-09-11 12:24:42 +0300

    relis 1

---
+++ src/alluser/AllUser.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package alluser;
+
+/**
+ *
+ * @author oleg
+ */
+public class AllUser {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        User u1 = new User(1, "user1", "passowrd1");
+        User u2 = new User(2, "user2", "passowrd2");
+        
+        
+        
+        // TODO code application logic here
+    }
+    
+}
---
+++ src/alluser/User.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package alluser;
+
+/**
+ *
+ * @author oleg
+ */
+public class User {
+    
+    int id;
+    String login;
+    String password;
+    
+    public User(int id, String login, String passowrd){
+        this.id = id;
+        this.login = login;
+        this.password = passowrd;
+   
+    }
+            
+            
+}
